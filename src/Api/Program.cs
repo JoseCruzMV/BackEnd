3cbbae3e8d7f6ed553320dc26abe552fec0555de
josecruz <josecruzmv@outlook.com>
2025-10-24 19:29:59 -0600

    Se creo el servicio para hacer la configuracion para inyectar los servicios.

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,5 +1,6 @@
 using System.Text;
 using Ecommerce.Domain;
+using Ecommerce.Infrastructure;
 using Ecommerce.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -11,6 +12,8 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddInfrastructureService(builder.Configuration);
 
 builder.Services.AddDbContext<EcommerceDbContext>(options =>
 {
