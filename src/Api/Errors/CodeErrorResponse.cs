1b0bc346f4b3d0ca0ee7c4f813dad1b172cb0e47
josecruz <josecruzmv@outlook.com>
2025-11-03 18:19:13 -0600

    Configuracion exceptions.

---
+++ src/Api/Errors/CodeErrorResponse.cs
@@ -0,0 +1,39 @@
+using Newtonsoft.Json;
+
+namespace Ecommerce.Api.Errors;
+
+public class CodeErrorResponse
+{
+    [JsonProperty(PropertyName = "statusCode")]
+    public int StatusCode { get; set; }
+    [JsonProperty(PropertyName = "message")]
+    public string[]? Message { get; set; }
+
+
+    public CodeErrorResponse(int statusCode, string[]? message = null)
+    {
+        StatusCode = statusCode;
+        if (message is null)
+        {
+            Message = Array.Empty<string>();
+            var text = GetDefaultMessageStatusCode(statusCode);
+            Message[0] = text;
+        }
+        else
+        {
+            Message = message;
+        }
+    }
+    
+    public string GetDefaultMessageStatusCode(int statusCode)
+    {
+        return statusCode switch
+        {
+            400 => "El request enviado tiene errores",
+            401 => "No tienes autorización para este recurso",
+            404 => "No se encontró el recurso solicitado",
+            500 => "Se produjeron errores en el servidor",
+            _=> string.Empty
+        };
+    }
+}
