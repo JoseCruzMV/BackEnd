5f08fe09dc0041ad7bef4ea48ef59e98b1ca2860
josecruz <josecruzmv@outlook.com>
2025-11-18 17:30:20 -0600

    Creados los vms para el mappeo

---
+++ src/Core/Ecommerce.Application/Models/Product/ProductStatusLabel.cs
@@ -0,0 +1,8 @@
+namespace Ecommerce.Application.Models.Product;
+
+public static class ProductStatusLabel
+{
+    public const string ACTIVO = nameof(ACTIVO);
+    public const string INACTIVO = nameof(INACTIVO);
+    public const string DESCONOCIDO = nameof(DESCONOCIDO);
+}
